e222fd4277b4101ae28df8a21bd3892017e5deb7
zhou_jianwen@qq.com <zhou_jianwen@qq.com>
2016-07-13 16:52:36 +0800

    commit

--- chapter06/src/chapter06/session/PurchaseServlet.java
+++ chapter06/src/chapter06/session/PurchaseServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.crypto.spec.IvParameterSpec;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.Cookie;
@@ -11,7 +12,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
 
 /**
  * Servlet implementation class PurchaseServlet
@@ -33,7 +33,7 @@ public class PurchaseServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
 		String id = req.getParameter("id");
-		
+
 		if (id == null) {
 			String url = "/chapter06/ListBookServlet";
 			resp.sendRedirect(url);
@@ -42,20 +42,24 @@ public class PurchaseServlet extends HttpServlet {
 
 		Book book = BookDB.getBook(id);
 		HttpSession session = req.getSession();
-		List<Book> cart = (List<Book>)session.getAttribute("cart");
-		if(cart==null)
-		{
+		List<Book> cart = (List<Book>) session.getAttribute("cart");
+		if (cart == null) {
 			cart = new ArrayList<Book>();
 			session.setAttribute("cart", cart);
 		}
-	
+
 		cart.add(book);
-		Cookie cookie = new Cookie("JSESSIONID", session.getId());
-		cookie.setMaxAge(60*30);
-		cookie.setPath("/chapter06");
-		resp.addCookie(cookie);
-		
+		/*
+		 * 以下这段代码 如果浏览器的Cookie功能被禁止，那么服务器端是无法通过Session保存用户会话信息的。 Cookie cookie =
+		 * new Cookie("JSESSIONID", session.getId()); cookie.setMaxAge(60*30);
+		 * cookie.setPath("/chapter06"); resp.addCookie(cookie);
+		 */
+
 		String url = "/chapter06/CartServlet";
+		/*
+		 * 考虑到浏览器可能不支持Cookie的情况，Servlet规范中还引入了URL重写机制来保存用户的会话信息。
+		 * 所谓URL重写，指的是将Session的会话标识号以参数的形式附加在超链接的URL地址后面。
+		 */
 		url = resp.encodeRedirectURL(url);
 		resp.sendRedirect(url);
 	}
