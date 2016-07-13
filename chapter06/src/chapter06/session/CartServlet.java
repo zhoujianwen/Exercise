e2ff3b88d6399f618994aa44c4314a6b2a7c43b3
zhou_jianwen@qq.com <zhou_jianwen@qq.com>
2016-07-13 16:39:58 +0800

    commit

--- chapter06/src/chapter06/session/CartServlet.java
+++ chapter06/src/chapter06/session/CartServlet.java
@@ -9,6 +9,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class CartServlet
@@ -16,31 +17,54 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/CartServlet")
 public class CartServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public CartServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public CartServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		doPost(request, response);
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		
 		response.setContentType("text/html;charset=utf-8");
 		PrintWriter out = response.getWriter();
 		List<Book> cart = null;
-		
+		boolean purFlag = true;
+		HttpSession session = request.getSession(false);
+		if (session == null) {
+			purFlag = false;
+		} else {
+			cart = (List) session.getAttribute("cart");
+			if (cart == null) {
+				purFlag = false;
+			}
+		}
+
+		if (!purFlag) {
+			out.write("对不起！您还没有购买任何商品！<br>");
+		} else {
+			out.write("您购买的图书有：<br>");
+			//double price = 0;
+			for (Book book : cart) {
+				out.write(book.getName() + "<br>");
+			}
+		}
 	}
 
 }
